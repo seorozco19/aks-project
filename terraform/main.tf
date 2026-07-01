a255647bb17657da24f53c23d13d6ed533eb78b3
sebastian <joan.orozco@globant.com>
2026-07-01 13:10:54 -0500

    update terraform aks

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   dns_prefix          = "${var.prefix}-k8s"
+  oidc_issuer_enabled = true
+  workload_identity_enabled = true
 
   default_node_pool {
     name       = "default"
